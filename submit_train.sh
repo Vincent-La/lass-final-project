d96ac4a4f94deb147967d2336f1edc6e6de38021
Vincent La <vincentla20877@gmail.com>
2024-11-22 16:00:51 -0500

    modify submit training script

--- submit_train.sh
+++ submit_train.sh
@@ -3,16 +3,16 @@
 #SBATCH --job-name=audiosep_onepeace_train                     # sets the job name
 #SBATCH --output=audiosep_onepeace_train.%j                    # indicates a file to redirect STDOUT to; %j is the jobid. If set, must be set to a file instead of a directory or else submission will fail.
 #SBATCH --error=audiosep_onepeace_train.%j                     # indicates a file to redirect STDERR to; %j is the jobid. If set, must be set to a file instead of a directory or else submission will fail.
-#SBATCH --time=24:00:00                                      # how long you would like your job to run; format=hh:mm:ss
+#SBATCH --time=72:00:00                                      # how long you would like your job to run; format=hh:mm:ss
 
-#SBATCH --partition=class
-#SBATCH --qos=high                                 # set QOS, this will determine what resources can be requested
-#SBATCH --account=class
-#SBATCH --gres=gpu:rtxa5000:4
+#SBATCH --partition=vulcan-scavenger
+#SBATCH --qos=vulcan-scavenger                                # set QOS, this will determine what resources can be requested
+#SBATCH --account=vulcan-abhinav
+#SBATCH --gres=gpu:rtxa6000:1
 
 #SBATCH --nodes=1                                               # number of nodes to allocate for your job
-#SBATCH --ntasks=4                                              
-#SBATCH --ntasks-per-node=4                                      
+#SBATCH --ntasks=1                                             
+#SBATCH --ntasks-per-node=1                                     
 #SBATCH --mem=128gb                                               # (cpu) memory required by job; if unit is not specified MB will be assumed
 
 module load cuda
